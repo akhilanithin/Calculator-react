01b2c1257563bc98bf1cbd8d7e9a1755fb3efd2a
akhilanithin <GFGexample@gmail.orgg>
2023-11-01 15:38:47 +0530

    React Calculator

---
+++ src/Tittle.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import logo from './logo.png';
+
+export const Tittle = () => {
+  return (
+    <div className='Title'>
+    <img src={logo} alt="logo" /><span className='Name'>REACT CALCULATOR</span>
+    </div>
+  )
+}
